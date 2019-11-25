9f0930ba965c8a321f99630e8dc3f7262cfc6dfb
Madinanachan <52584978+Madinanachan@users.noreply.github.com>
2019-11-25 12:47:16 -0800

    Update insights.py

--- pages/insights.py
+++ pages/insights.py
@@ -12,14 +12,9 @@ from app import app
 # https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
 column1 = dbc.Col(
     [
-        dcc.Markdown(
-            """
-        
-            ## Insights
-
-
-            """
-        ),
+        dcc.Markdown('### An Interesting Trend'),
+        dcc.Markdown('##### Some data is not always helpful-- this was exemplified by one of the features I tried to scrape from Leafly. Turns out the most treated complaint pot smokers have is Stress. No Matter what else they had going on; depression, insomnia, anxiety, pain-- one thing remained true. They were all stressed and treating it with cannabis.'),
+        html.Img(src='assets/Stressedout.jpg',className='img-fluid', style = {'height': '400px'})
 
     ],
 )
