540dfbe7ff45f836e2c6e9680455de9d4b1e2d07
Madinanachan <52584978+Madinanachan@users.noreply.github.com>
2019-11-25 12:46:36 -0800

    Update process.py

--- pages/process.py
+++ pages/process.py
@@ -12,16 +12,21 @@ from app import app
 # https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
 column1 = dbc.Col(
     [
-        dcc.Markdown(
-            """
-        
-            ## Process
-
-
-            """
-        ),
+        dcc.Markdown( '### Dataset and Model'),
+        dcc.Markdown('##### The dataset was obtained off of the popular cannabis webcite leafly via symptom releif and perceived effects from reviews of individual strains. For the Baseline model Hybrid was predicted everytime for ~49% accuracy. The final model chosen was an Ordinally Encoded Decision Tree, as its ROC AUC score was best at 75%--more than beating baseline.'),
+        dcc.Markdown('##### Several features were enginered; Tolerance, Energy, and Discomfort. These helped narrow in on strain types. Using Feature Importance graphs to judge, irrelevant features were discluded for the success of the model. '),
+        html.Img(src='assets/Permutation.jpg',className='img-fluid',style={'height':'200px'}),
 
     ],
 )
 
-layout = dbc.Row([column1])
+column2= dbc.Col(
+    [
+        dcc.Markdown('### Feature Importance'),
+        html.Img(src='assets/FI4.jpg',className='img-fluid', style = {'height': '200px'}),
+        html.Img(src='assets/Featureimportance2.jpg',className='img-fluid', style = {'height': '200px'}),
+        dcc.Markdown('##### Feature Importance and Permutation Importance show the relative importance of the features to the overall model.')
+    ]
+)
+
+layout = dbc.Row([column1,column2])
